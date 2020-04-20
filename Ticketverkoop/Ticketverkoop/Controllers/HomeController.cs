26131266b206ad6c98cd8e1faf926adb9bb7674a
Sammy.Lok <katjonsammy.lok@student.vives.be>
2020-04-20 13:45:44 +0200

    Mail versturen via contactpagina werkt

--- Ticketverkoop/Ticketverkoop/Controllers/HomeController.cs
+++ Ticketverkoop/Ticketverkoop/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,46 @@ namespace Ticketverkoop.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactVM contactVM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage message = new MailMessage();
+                    message.From = new MailAddress(contactVM.Email);
+
+                    message.To.Add("sammy.mvccore@gmail.com");
+                    message.Subject = "Contact pagina van " + contactVM.Naam + " " + contactVM.Voornaam;
+                    message.Body = contactVM.Message;
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential("sammy.mvccore@gmail.com", "azerty-123");
+                    smtp.EnableSsl = true;
+                    smtp.Send(message);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Bericht is verstuurd. We gaan u zo snel mogelijk contacteren.";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Bericht kan niet verstuurd worden, probeer het later nog een keer.: {ex.Message}";
+                }
+            }
+
+            return View();
         }
     }
 }
---
+++ Ticketverkoop/Ticketverkoop/ViewModels/ContactVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ticketverkoop.ViewModels
+{
+    public class ContactVM
+    {
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "De naam moet minstens 3 en maximum 50 karakters bevatten.")]
+        public string Naam { get; set; }
+
+        [Required(ErrorMessage = "Voornaam is verplicht.")]
+        public string Voornaam { get; set; }
+
+        [Required(ErrorMessage = "E-mailadres is verplicht.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Vul een geldig e-mailadres in.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Het berichtveld is verplicht.")]
+        public string Message { get; set; }
+    }
+}
